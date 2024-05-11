93102644c82cfe3f5d9bd404d0cf5afd2cd8f0a7
Isaac.Zarspada <IsaacZarspada@gmail.com>
2024-05-11 13:42:02 -0500

    added sprint 1 items

---
+++ classes/square.py
@@ -0,0 +1,10 @@
+import pygame as pg
+
+
+class Player:
+
+    pg.surface( )
+
+
+# surface | color | rect (Rect) | width (int) --
+
--- main.py
+++ main.py
@@ -12,17 +12,24 @@ runtime = True
 while runtime: #while statement will continue until statement is false
     for event in pg.event.get(): #event.get is grabbing all events during runtime, then we are looping through those events
         if event.type == pg.QUIT: #this event refers to x button on a window
-            running = False
+            runtime = False
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("cyan")
 
     # RENDER YOUR GAME HERE
+    
+
+
 
     # flip() the display to put your work on screen
     pg.display.flip()
 
     clock.tick(60)  # limits FPS to 60
+
+
+
+
 
 
 pg.quit()
