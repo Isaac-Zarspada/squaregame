1d892dda3754e800a50f76ad31032c6a35d0c00f
Isaac.Zarspada <IsaacZarspada@gmail.com>
2024-05-12 16:09:10 -0500

    added movement up and down

--- classes/square.py
+++ classes/square.py
@@ -1,7 +1,8 @@
 import pygame as pg
 
 class Player:
-    COLOR = (255,0,0)
+    COLOR_RED = (255,0,0)
+    COLOR_BLUE = (0,0,255)
     def __init__(self, x, y, width, height):
         self.rect = pg.Rect(x,y,width,height)
         self.velx = 0
@@ -17,6 +18,12 @@ class Player:
     def move_right(self, vel):
         self.velx = vel
 
+    def move_up(self, vel):
+        self.velx = -vel
+
+    def move_down(self, vel):
+        self.velx = vel
+
     def display(self, screen):
         screen.blit(self.surf,self.rect)
     
@@ -24,4 +31,4 @@ class Player:
         self.move(self.velx, self.vely)
 
     def draw(self, screen):
-        pg.draw.rect(screen, self.COLOR, self.rect)
+        pg.draw.rect(screen, self.COLOR_BLUE, self.rect)
